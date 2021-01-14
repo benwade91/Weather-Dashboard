ef72ab3a628435108fa316053de63926aedeccc3
benwade91 <70988204+benn925@users.noreply.github.com>
2021-01-13 16:05:13 -0800

    app communicates with weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,30 @@
 var apiKey = "411165753b54ef779d0c21d853c22321";
 var city = "london";
-var getWeather = function(city) {
-    // format the github api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
+
+var cityInputEl = document.querySelector("#citySearch");
+var cityFormEl = document.querySelector("#cityForm");
+
+var citySubmitHandler = function(event) {
+    
+    event.preventDefault();
   
-    // make a get request to url
+    var city = cityInputEl.value.trim();
+  
+    if (city) {
+      console.log(city);
+      cityInputEl.value = "";
+    } else {
+      alert("Please enter a city");
+    }
+  };
+
+var getWeather = function(city) {
+    
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
+
     fetch(apiUrl)
       .then(function(response) {
-        // request was successful
+          
         if (response.ok) {
           console.log(response);
           response.json().then(function(data) {
@@ -22,3 +39,4 @@ var getWeather = function(city) {
       });
   };
   getWeather(city);
+  cityFormEl.addEventListener("submit", citySubmitHandler);
